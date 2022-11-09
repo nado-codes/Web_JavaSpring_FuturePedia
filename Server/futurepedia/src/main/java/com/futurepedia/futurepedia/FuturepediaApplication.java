ac61dc3816f3925b5196f7902b6d8478539e8c52
nathnado22 <67047470+nathnado22@users.noreply.github.com>
2022-11-09 11:05:17 +0800

    Working on Setup + First Controller

---
+++ Server/futurepedia/src/main/java/com/futurepedia/futurepedia/FuturepediaApplication.java
@@ -0,0 +1,13 @@
+package com.futurepedia.futurepedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuturepediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuturepediaApplication.class, args);
+	}
+
+}
