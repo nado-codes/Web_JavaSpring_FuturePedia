daf2826d8745bd51bcd09616c8977038780c13a3
nathnado22 <67047470+nathnado22@users.noreply.github.com>
2022-11-11 08:05:34 +0800

    Fixed GreetingController not mapped correctly
    ... Turned out that the main app class was in the wrong package, so com.futurepedia.futurepedia -> com.futurepedia fixed the issue, as the greeting controller is under com.futurepedia.models

--- Server/futurepedia/src/main/java/com/futurepedia/FuturepediaApplication.java
+++ Server/futurepedia/src/main/java/com/futurepedia/FuturepediaApplication.java
@@ -1,4 +1,4 @@
-package com.futurepedia.futurepedia;
+package com.futurepedia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
