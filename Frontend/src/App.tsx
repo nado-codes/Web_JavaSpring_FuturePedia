b8ac78e694547c8627be6650191dcc6ac809d478
nathnado22 <67047470+nathnado22@users.noreply.github.com>
2022-11-12 17:40:40 +0800

    Added react router and started working on main page stubs

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./Views/Home";
+import { SnackbarProvider } from "notistack";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SnackbarProvider maxSnack={4} autoHideDuration={2000}>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="home" element={<Home />} />
+            <Route path="*" element={<h1>U fkn w0t m8?</h1>} />
+          </Routes>
+        </BrowserRouter>
+      </SnackbarProvider>
     </div>
   );
 }
---
+++ Frontend/src/Views/404.tsx
@@ -0,0 +1,1 @@
+export const _404: React.FC = () => <></>;
