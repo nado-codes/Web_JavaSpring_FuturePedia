0820b00ce6ff4e008baeb9077233ef5646cb82b1
nathnado22 <67047470+nathnado22@users.noreply.github.com>
2022-11-14 06:57:47 +0800

    MySQL Database Setup
    + Added MySQL Connector Dependency to Maven
    + Setup proc for AddArticle
    
    ~ Modified columns "Id" in "Articles" table to auto increment and "LastModified" to use CURRENT_TIMESTAMP for default value and when updated*
    *still to be tested
    
    ! Fixed issues with SELECT procs using parenthesis around column names when not required, resulting in SQLException in e.g. GetArticles "Operand should contain one column(s)"

--- SQL/Futurepedia_1.sql
+++ SQL/Futurepedia_1.sql
@@ -5,36 +5,35 @@ USE Futurepedia;
 
 /* ARTICLES */
 CREATE TABLE Articles (
-	Id int not null primary key,
+	Id int not null primary key AUTO_INCREMENT,
     Name varchar(64) not null,
     Content text not null,
 	DateAdded datetime not null DEFAULT NOW(),
-	LastModified timestamp not null
+	LastModified timestamp not null DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 DELIMITER //
 CREATE PROCEDURE GetArticles()
 BEGIN
-	SELECT (Id, Name, Content, DateAdded, LastModified) FROM Articles;
+	SELECT Id, Name, Content, DateAdded, LastModified FROM Articles;
 END //
 DELIMITER //
 CREATE PROCEDURE GetArticleById(IN id int)
 BEGIN
-	SELECT (Id, Name, Content, DateAdded, LastModified) FROM Articles WHERE Id=id;
+	SELECT Id, Name, Content, DateAdded, LastModified FROM Articles WHERE Id=id;
 END //
 CREATE PROCEDURE GetArticleByName(IN name varchar(64))
 BEGIN
-	SELECT (Id, Name, Content, DateAdded, LastModified) FROM Articles WHERE Name=name;
+	SELECT Id, Name, Content, DateAdded, LastModified FROM Articles WHERE Name=name;
 END //
 CREATE PROCEDURE AddArticle(
 	IN name varchar(64),
     IN content text
 )
 BEGIN
-	INSERT INTO Articles (Id, Name, Content, DateAdded, LastModified)
-    VALUES(Name,Content);
+	INSERT INTO Articles (Name, Content)
+    VALUES(name,content);
 END //
 	
-
 /* ROLLBACK
 DROP DATABASE Futurepedia
 */
